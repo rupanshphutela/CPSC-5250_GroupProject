d18b71b923e44f798325c40a8353d0f57b5113fb
Meryll Cruz <mcruz3@seattleu.edu>
2023-02-10 14:24:43 -0800

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Applications/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/meryllcruz/Downloads/tinderCloneUI-master"
+export "FLUTTER_APPLICATION_PATH=/Users/meryllcruz/Dig"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
