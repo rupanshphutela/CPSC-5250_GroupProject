1afca021bb6ed7a0622be5071de4a667a1f1ca1a
Meryll Cruz <mcruz3@seattleu.edu>
2023-02-10 22:06:38 -0800

    Added swiping gesture up to feedback_position_provider.dart

--- lib/provider/feedback_position_provider.dart
+++ lib/provider/feedback_position_provider.dart
@@ -1,31 +1,50 @@
 import 'package:flutter/material.dart';
 
-enum SwipingDirection { left, right, none }
+enum SwipingDirection { left, right, up, down, none }
 
 class FeedbackPositionProvider extends ChangeNotifier {
   double _dx = 0.0;
-  SwipingDirection? _swipingDirection;
+  double _dy = 0.0;
+  SwipingDirection _swipingDirection = SwipingDirection.none;
 
-  SwipingDirection? get swipingDirection => _swipingDirection;
+  SwipingDirection get swipingDirection => _swipingDirection;
+  double get dx => _dx;
+  double get dy => _dy;
 
   FeedbackPositionProvider() {
     _swipingDirection = SwipingDirection.none;
+    _dx = 0.0;
+    _dy = 0.0;
   }
 
   void resetPosition() {
     _dx = 0.0;
+    _dy = 0.0;
     _swipingDirection = SwipingDirection.none;
     notifyListeners();
   }
 
-  void updatePosition(double changeInX) {
+  void updatePosition(double changeInX, double changeInY) {
     _dx = _dx + changeInX;
-    if (_dx > 0) {
-      _swipingDirection = SwipingDirection.right;
-    } else if (_dx < 0) {
-      _swipingDirection = SwipingDirection.left;
+    _dy = _dy + changeInY;
+    if (_dx.abs() > _dy.abs()) {
+      // feedback in x direction dominates
+      if (_dx > 0) {
+        _swipingDirection = SwipingDirection.right;
+      } else if (_dx < 0) {
+        _swipingDirection = SwipingDirection.left;
+      } else {
+        _swipingDirection = SwipingDirection.none;
+      }
     } else {
-      _swipingDirection = SwipingDirection.none;
+      // feedback in y direction dominates
+      if (_dy > 0) {
+        _swipingDirection = SwipingDirection.down;
+      } else if (_dy < 0) {
+        _swipingDirection = SwipingDirection.up;
+      } else {
+        _swipingDirection = SwipingDirection.none;
+      }
     }
     notifyListeners();
   }
