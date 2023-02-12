d70b16d92a7c47d01c63c4e2858895043b892bf9
Yesha Sharma <yeshasharma1996@gmail.com>
2023-02-11 20:34:17 -0800

    Loaded few dependencies and router setup with bottom navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:my_dig_app/page/home_page.dart';
-import 'package:provider/provider.dart';
-import 'package:my_dig_app/provider/feedback_position_provider.dart';
+import 'package:go_router/go_router.dart';
+import 'package:the_dig_app/screens/chat.dart';
+import 'package:the_dig_app/screens/dog_profile.dart';
+import 'package:the_dig_app/screens/event.dart';
+import 'package:the_dig_app/screens/settings.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+final _routes = [
+    GoRoute(
+      path: '/dogprofile',
+      builder: (context, state) => const DogProfile(),
+    ),
+    GoRoute(
+      path: '/chats',
+      builder: (context, state) => const Chat(),
+    ),
+    GoRoute(
+      path: '/events',
+      builder: (context, state) => const Event(),
+    ),
+    GoRoute(
+      path: '/settings',
+      builder: (context, state) => const Settings(),
+    ),
+];
+
+final _router = GoRouter(
+  initialLocation: '/dogprofile',
+  routes: _routes,
+);
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => FeedbackPositionProvider(),
-        child: MaterialApp(
-          title: 'My Dig App',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          home: HomePage(),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      routerConfig: _router,
+    );
+  }
 }
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DIG"),
+      ),
+      body: const DogProfile(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.teal), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat, color: Colors.teal), label: 'Chats'),
+          BottomNavigationBarItem(icon: Icon(Icons.event, color: Colors.teal), label: 'Events'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings, color: Colors.teal), label: 'Settings'),
+        ],
+        onTap: (index){
+          context.push(_routes[index].path);
+        },
+      ),
+    );
+  }
+}
+
