903bee8483e52fa54826d9c1e19bfa28b8b60732
Rupansh Phutela <rupanshphutela@gmail.com>
2023-02-12 13:57:28 -0800

    Floor - Read all profiles database query

--- lib/models/profile_dao.dart
+++ lib/models/profile_dao.dart
@@ -6,6 +6,9 @@ abstract class ProfileDao {
   @Query("SELECT * FROM profile WHERE id = :profileId")
   Future<Profile?> getProfileByProfileId(String profileId);
 
+  @Query("SELECT * FROM profile")
+  Future<List<Profile>?> getAllProfiles();
+
   @insert
   Future<void> insertProfile(Profile profile);
 
