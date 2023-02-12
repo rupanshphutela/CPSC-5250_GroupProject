9a7eabc342f415467715bd38d72e65224e2f6bd4
Yesha Sharma <yeshasharma1996@gmail.com>
2023-02-11 20:38:16 -0800

    ADD: ProfileCard on the profile page.

---
+++ lib/util/profile_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../models/profile_model.dart';
+
+class ProfileCard extends StatelessWidget {
+  final ProfileModel card;
+
+  const ProfileCard({super.key, required this.card});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          )
+        ],
+      ),
+      alignment: Alignment.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: card.color,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  card.name,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  card.breed,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  card.location,
+                  style: const TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
