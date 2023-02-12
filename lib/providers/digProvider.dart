985e8d2ea01aed24daa7007995ada69e6636222f
Yesha Sharma <yeshasharma1996@gmail.com>
2023-02-12 15:07:54 -0800

    ADD: Setup CRUD through provider.

--- lib/providers/digProvider.dart
+++ lib/providers/digProvider.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:the_dig_app/models/app_database.dart';
+import 'package:the_dig_app/models/profile.dart';
+import 'dart:async';
 
 class DigProvider extends ChangeNotifier {
   final AppDatabase _database;
 
   DigProvider(this._database);
+
+
+
+  Future<void> insertProfile(Profile profile) async {
+    await _database.profileDao.insertProfile(profile);
+        final result = await _database.database.rawQuery(
+        'SELECT * FROM profile'
+    );
+    print(result.length);
+    notifyListeners();
+  }
+
+    Future<List<Profile>?> getProfiles() => _database.profileDao.getAllProfiles();
+  
 }
