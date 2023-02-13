0cb8395c3024c51a8b226af975f1311aa019ffb7
Yesha Sharma <yeshasharma1996@gmail.com>
2023-02-12 22:43:24 -0800

    ADD: Implemented insertion in provider for fetching left and right swipe.

--- lib/providers/digProvider.dart
+++ lib/providers/digProvider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:the_dig_app/models/app_database.dart';
+import 'package:the_dig_app/models/left_swipe.dart';
 import 'package:the_dig_app/models/owner.dart';
 import 'package:the_dig_app/models/profile.dart';
 import 'dart:async';
+
+import 'package:the_dig_app/models/right_swipe.dart';
 
 class DigProvider extends ChangeNotifier {
   final AppDatabase _database;
@@ -14,12 +17,19 @@ class DigProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-
-
-
   Future<void> insertProfile(Profile profile) async {
     await _database.profileDao.insertProfile(profile);
     notifyListeners();
+  }
+
+  Future<void> insertLeftSwipe(LeftSwipe leftSwipe) async {
+    await _database.leftSwipeDao.insertLeftSwipe(leftSwipe);
+    notifyListeners();    
+  }
+
+  Future<void> insertRightSwipe(RightSwipe rightSwipe) async {
+    await _database.rightSwipeDao.insertRightSwipe(rightSwipe);
+    notifyListeners();    
   }
 
     Future<List<Profile>> getProfiles() => _database.profileDao.getAllProfiles();
