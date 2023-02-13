381aa176e54c157c3e63720888a46731e4f84385
Yesha Sharma <yeshasharma1996@gmail.com>
2023-02-12 18:59:08 -0800

    ADD: Setup provider for insertion of owner and profile .

--- lib/providers/digProvider.dart
+++ lib/providers/digProvider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:the_dig_app/models/app_database.dart';
+import 'package:the_dig_app/models/owner.dart';
 import 'package:the_dig_app/models/profile.dart';
 import 'dart:async';
 
@@ -8,17 +9,19 @@ class DigProvider extends ChangeNotifier {
 
   DigProvider(this._database);
 
+  Future<void> insertOwnerProfile(Owner owner) async {
+    await _database.ownerProfileDao.insertOwnerProfile(owner);
+    notifyListeners();
+  }
+
+
 
 
   Future<void> insertProfile(Profile profile) async {
     await _database.profileDao.insertProfile(profile);
-        final result = await _database.database.rawQuery(
-        'SELECT * FROM profile'
-    );
-    print(result.length);
     notifyListeners();
   }
 
-    Future<List<Profile>?> getProfiles() => _database.profileDao.getAllProfiles();
+    Future<List<Profile>> getProfiles() => _database.profileDao.getAllProfiles();
   
 }
