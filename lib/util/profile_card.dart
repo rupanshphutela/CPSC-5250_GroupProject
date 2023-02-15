7bfee0b9ad66531f130aa50384122e6ee1a6d1ec
Meryll Cruz <mcruz3@seattleu.edu>
2023-02-14 17:36:24 -0800

    Refactored profile_card.dart

--- lib/util/profile_card.dart
+++ lib/util/profile_card.dart
@@ -1,112 +1,131 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:the_dig_app/models/profile.dart';
-import 'package:the_dig_app/providers/feedback_position_provider.dart';
+import 'package:the_dig_app/screens/dog_profile.dart';
 
 class ProfileCard extends StatelessWidget {
   final Profile card;
-  final bool isUserinFocus;
 
   const ProfileCard({
     required this.card,
-    required this.isUserinFocus,
     Key? key,
   }) : super(key: key);
+
+  get direction => null;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final provider = Provider.of<FeedbackPositionProvider>(context);
-    final swipingDirection = provider.swipingDirection;
     return Container(
-      height: size.height * 0.8,
-      width: size.width * 0.95,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        image: DecorationImage(
-          image: AssetImage(card.profilePicture),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Container(
+        height: size.height * 0.8,
+        width: size.width * 0.95,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(color: Colors.black12, spreadRadius: 0.5),
-          ],
-          gradient: const LinearGradient(
-            colors: [Colors.black12, Colors.black87],
-            begin: Alignment.center,
-            stops: [0.4, 1],
-            end: Alignment.bottomCenter,
+          image: DecorationImage(
+            image: AssetImage(card.profilePicture),
+            fit: BoxFit.cover,
           ),
         ),
-        child: Stack(
-          children: [
-            Positioned(
-              right: 10,
-              left: 10,
-              bottom: 10,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildUserInfo(card: card),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16, right: 8),
-                    child: Icon(Icons.info, color: Colors.white),
-                  )
-                ],
-              ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(color: Colors.black12, spreadRadius: 0.5),
+            ],
+            gradient: const LinearGradient(
+              colors: [Colors.black12, Colors.black87],
+              begin: Alignment.center,
+              stops: [0.4, 1],
+              end: Alignment.bottomCenter,
             ),
-            if (isUserinFocus) buildLikeBadge(swipingDirection)
-          ],
-        ),
-      ),
-    );
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                right: 10,
+                left: 10,
+                bottom: 10,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    buildUserInfo(card: card),
+                    buildLikeBadge(direction: direction),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 16, right: 8),
+                      child: Icon(Icons.info, color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 
-  Widget buildLikeBadge(SwipingDirection swipingDirection) {
-    final isSwipingRight = swipingDirection == SwipingDirection.right;
-    final isSwipingUp = swipingDirection == SwipingDirection.up;
-    final color = isSwipingRight ? Colors.green : Colors.pink;
-    final angle = isSwipingRight ? -0.5 : 0.5;
-
-    if (swipingDirection == SwipingDirection.none) {
-      return Container();
-    } else {
+  Widget buildLikeBadge({required CardSwiperDirection? direction}) {
+    if (direction?.name == 'right') {
       return Positioned(
-        top: isSwipingUp ? null : 20,
-        right: isSwipingRight ? null : 20,
-        left: isSwipingRight ? 20 : null,
-        child: Transform.rotate(
-          angle: angle,
-          child: Container(
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: color, width: 2),
-            ),
-            child: Text(
-              _getMessage(isSwipingUp, isSwipingRight),
-              style: TextStyle(
-                color: color,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+          child: Transform.rotate(
+        angle: 0.5,
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.green, width: 2),
+          ),
+          child: const Text(
+            'DIG',
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
-      );
-    }
-  }
-
-  String _getMessage(bool isSwipingUp, bool isSwipingRight) {
-    if (isSwipingUp) {
-      return 'SUPER DIG';
-    } else if (isSwipingRight) {
-      return 'DIG';
+      ));
+    } else if (direction?.name == 'left') {
+      return Positioned(
+          child: Transform.rotate(
+        angle: 0.5,
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.pink, width: 2),
+          ),
+          child: const Text(
+            'WOOF',
+            style: TextStyle(
+              color: Colors.pink,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ));
+    } else if (direction?.name == 'top') {
+      return Positioned(
+          child: Transform.rotate(
+        angle: 0.5,
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.pink, width: 2),
+          ),
+          child: const Text(
+            'SUPER DIG',
+            style: TextStyle(
+              color: Colors.pink,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ));
     } else {
-      return 'WOOF';
+      return Visibility(
+        visible: false,
+        child: Container(),
+      );
     }
   }
 
@@ -117,7 +136,7 @@ class ProfileCard extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '${card.fName}, ${card.breed}',
+              card.fName,
               style: const TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
@@ -126,7 +145,7 @@ class ProfileCard extends StatelessWidget {
             ),
             const SizedBox(height: 8),
             Text(
-              card.gender,
+              '${card.gender}, ${card.breed}',
               style: const TextStyle(color: Colors.white),
             ),
             const SizedBox(height: 4),
