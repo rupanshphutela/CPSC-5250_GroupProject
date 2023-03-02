c580ef1896b88e6a4a48947b602ffadc8dc072e3
Yesha Sharma <yeshasharma1996@gmail.com>
2023-03-01 17:33:30 -0800

    [FIX] Modified the profile with all details.

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -4,8 +4,13 @@ import 'package:floor/floor.dart';
 class Profile {
   @primaryKey
   int id;
+  String ownerfName;
+  String ownerlName;
+  String email;
+  int phone;
+  String city;
+  String ownerprofilePicture;
   String fName;
-  String lName;
   String profilePicture;
   int ownerId;
   String? biography;
@@ -14,35 +19,69 @@ class Profile {
   String color;
   bool isVaccinated;
   String registrationDate;
-  bool isSpayed;
-  bool isNeutered;
+  bool? isSpayed;
+  bool? isNeutered;
   String joiningDate;
   String size;
+  int? socialIndexHumans;
+  int? socialIndexDogs;
+  bool? isFoodAggressive;
+  bool? isNewHumanAggressive;
+  bool? isNewDogAggressive;
+  String? foodName;
+  int? foodLikingIndex;
+  String? activityName;
+  int? activityLikingIndex;
+  String? skillName;
+  String? skillProficiency;
+
+
   Profile({
     required this.id,
-    required this.fName,
-    required this.lName,
-    required this.profilePicture,
     required this.ownerId,
+    required this.ownerfName,
+    required this.ownerlName,
+    required this.email,
+    required this.phone,
+    required this.city,
+    required this.ownerprofilePicture,
+    required this.fName,
+    required this.profilePicture,
     this.biography,
     required this.gender,
     required this.breed,
     required this.color,
     required this.isVaccinated,
     required this.registrationDate,
-    required this.isSpayed,
-    required this.isNeutered,
+    this.isSpayed,
+    this.isNeutered,
     required this.joiningDate,
     required this.size,
+    this.socialIndexHumans,
+    this.socialIndexDogs,
+    this.isFoodAggressive,
+    this.isNewHumanAggressive,
+    this.isNewDogAggressive,
+    this.foodName,
+    this.foodLikingIndex,
+    this.activityName,
+    this.activityLikingIndex,
+    this.skillName,
+    this.skillProficiency,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
+  Map<String, dynamic> toJson() => {
       'id': id,
-      'fName': fName,
-      'lName': lName,
-      'profilePicture': profilePicture,
       'ownerId': ownerId,
+      'ownerfName': ownerfName,
+      'lName': ownerlName,
+      'ownerlName': profilePicture,
+      'email': email,
+      'phone': phone,
+      'city': city,
+      'ownerprofilePicture': ownerprofilePicture,
+      'fName': fName,
+      'profilePicture': profilePicture,
       'biography': biography,
       'gender': gender,
       'breed': breed,
@@ -53,11 +92,21 @@ class Profile {
       'isNeutered': isNeutered,
       'joiningDate': joiningDate,
       'size': size,
-    };
-  }
+      'socialIndexHumans': socialIndexHumans,
+      'socialIndexDogs': socialIndexDogs,
+      'isFoodAggressive': isFoodAggressive,
+      'isNewHumanAggressive': isNewHumanAggressive,
+      'isNewDogAggressive': isNewDogAggressive,
+      'foodName': foodName,
+      'foodLikingIndex': foodLikingIndex,
+      'activityName': activityName,
+      'activityLikingIndex': activityLikingIndex,
+      'skillName': skillName,
+      'skillProficiency': skillProficiency,
+  };
 
-  @override
-  String toString() {
-    return 'Dog{id: $id, fName: $fName, lName: $lName, profilePicture: $profilePicture, ownerId: $ownerId, biography: $biography, gender: $gender}';
-  }
+  // @override
+  // String toString() {
+  //   return 'Dog{id: $id, fName: $fName, lName: $lName, profilePicture: $profilePicture, ownerId: $ownerId, biography: $biography, gender: $gender}';
+  // }
 }
