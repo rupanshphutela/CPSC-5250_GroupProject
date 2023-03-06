8fab08b7df6fec31592c3f1d733f5cb9f0de7b57
Rupansh Phutela <rupanshphutela@gmail.com>
2023-03-06 14:38:29 -0800

    Add - Settings Page changes to redirect to swipes

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:the_dig_app/providers/dig_firebase_provider.dart';
-import 'package:the_dig_app/routes/routes.dart';
 import 'package:the_dig_app/util/bottom_navigation_bar.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -11,10 +11,11 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<DigFirebaseProvider>(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("To be done later..."),
+        title: const Icon(Icons.settings),
       ),
       body: SettingsList(
         sections: [
@@ -22,24 +23,24 @@ class SettingsPage extends StatelessWidget {
             title: const Text('My Activities'),
             tiles: [
               SettingsTile(
-                title: const Text('Rejects'),
+                title: const Text('Left Swipes'),
                 leading: const Icon(Icons.arrow_back_ios),
                 onPressed: (BuildContext context) {
-                  // context.push('/left_swipe');
+                  context.push('/swipe?email=$email&direction=left');
                 },
               ),
               SettingsTile(
-                title: const Text('Likes'),
+                title: const Text('Right Swipes'),
                 leading: const Icon(Icons.arrow_forward_ios),
                 onPressed: (BuildContext context) {
-                  // context.push('/right_swipe');
+                  context.push('/swipe?email=$email&direction=right');
                 },
               ),
               SettingsTile(
-                title: const Text('Superlikes'),
+                title: const Text('Top Swipes'),
                 leading: const Icon(Icons.arrow_upward_outlined),
                 onPressed: (BuildContext context) {
-                  // context.push('/top_swipe');
+                  context.push('/swipe?email=$email&direction=top');
                 },
               ),
             ],
