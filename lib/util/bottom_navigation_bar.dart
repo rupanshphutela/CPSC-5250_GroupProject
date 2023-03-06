d5e57796eead3520c70e0604772b30f0f43fc0c9
Rupansh Phutela <rupanshphutela@gmail.com>
2023-03-06 14:09:17 -0800

    Add - Dig Bottom Navigation Bar component for reusability

---
+++ lib/util/bottom_navigation_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class DigBottomNavBar extends StatelessWidget {
+  final String email;
+
+  const DigBottomNavBar({
+    required this.email,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            color: Colors.teal,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.chat,
+            color: Colors.teal,
+          ),
+          label: 'Chats',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+            color: Colors.teal,
+          ),
+          label: 'Add Profile Form',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.settings,
+            color: Colors.teal,
+          ),
+          label: 'Settings',
+        ),
+      ],
+      onTap: (index) {
+        if (index == 0) {
+        } else if (index == 1) {
+        } else if (index == 2) {
+          context.push('/add/owner/profile?email=$email');
+        } else if (index == 3) {
+          context.push('/settings?email=$email');
+        }
+      },
+    );
+  }
+}
