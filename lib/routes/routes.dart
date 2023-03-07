9fc7d0ac326d51f31d5c7b53099eda5d1ded8869
Rupansh Phutela <rupanshphutela@gmail.com>
2023-03-06 23:51:52 -0800

    Added - route for incoming requests(top/right)

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:the_dig_app/screens/incoming_swipes_page.dart';
 import 'package:the_dig_app/screens/owner_profile_form.dart';
 import 'package:the_dig_app/screens/swipes_page.dart';
 import '../screens/profiles_page.dart';
@@ -46,6 +47,13 @@ final routes = [
       final String direction = state.queryParams['direction'].toString();
       final String email = state.queryParams['email'].toString();
       return SwipesPage(email: email, direction: direction);
+    },
+  ),
+  GoRoute(
+    path: '/incoming/swipes',
+    builder: (context, state) {
+      final String email = state.queryParams['email'].toString();
+      return IncomingSwipesPage(email: email);
     },
   ),
 ];
