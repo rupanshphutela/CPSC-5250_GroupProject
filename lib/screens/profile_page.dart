cdd0575288d0d0a0fa69da44ac2cf634c97d4d64
Yesha Sharma <yeshasharma1996@gmail.com>
2023-03-07 17:12:07 -0800

    [ADD] Added Display page for profile

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ProfilePage extends StatefulWidget {
   ProfilePage({super.key, required this.email});
@@ -10,8 +10,44 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  late FirebaseStorage _storage;
+
+  List<String> _imageUrls = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _storage = FirebaseStorage.instance;
+    _getImagesFromFirebaseStorage();
+  }
+
+  Future<void> _getImagesFromFirebaseStorage() async {
+    Reference ref = _storage.ref().child('images/');
+    ListResult result = await ref.listAll();
+
+    result.items.forEach((Reference ref) async {
+      String imageUrl = await ref.getDownloadURL();
+      setState(() {
+        _imageUrls.add(imageUrl);
+      });
+    });
+  }
+
+  @override
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image List'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(_imageUrls.length, (index) {
+          return Center(
+            child: Image.network(_imageUrls[index]),
+          );
+        }),
+      ),
+    );
   }
 }
