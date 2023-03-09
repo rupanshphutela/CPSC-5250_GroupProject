9b7e0e8a66aeedeb685abd496b8760a2020e62dc
zholdoshov <nur.joldoshov@gmail.com>
2023-03-08 20:00:28 -0800

    Getting started with chat

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:the_dig_app/models/contact.dart';
 import 'package:the_dig_app/util/bottom_navigation_bar.dart';
+
+import '../providers/dig_firebase_provider.dart';
 
 class Chat extends StatelessWidget {
   static const TextStyle optionStyle =
@@ -26,16 +32,43 @@ class Chat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final provider = Provider.of<DigFirebaseProvider>(context);
+    // List<Contact>? contacts = provider.contacts.toList();
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Icon(Icons.pets_outlined),
       ),
-      body: const Center(
-          child: Text(
-        'To be done later...',
-        style: optionStyle,
-      )),
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return ListView.builder(
+                padding: const EdgeInsets.all(10.0),
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Contact name'),
+                    subtitle: Text('Dog name'),
+                    trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.message)),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+      // const Center(
+      //     child: Text(
+      //   'To be done later...',
+      //   style: optionStyle,
+      // )),
       bottomNavigationBar: DigBottomNavBar(email: email),
     );
   }
