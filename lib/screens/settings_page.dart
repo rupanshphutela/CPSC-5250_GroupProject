e632b7835bbf3c8b0b3eb10d8f81aebaf163eeaa
Rupansh Phutela <rupanshphutela@gmail.com>
2023-03-08 23:49:19 -0800

    Add - Logout button on Settings Page and validation of login

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:the_dig_app/providers/dig_firebase_provider.dart';
+import 'package:the_dig_app/screens/login_page.dart';
 import 'package:the_dig_app/util/bottom_navigation_bar.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -12,66 +14,82 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<DigFirebaseProvider>(context);
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Icon(Icons.settings),
-      ),
-      body: SettingsList(
-        sections: [
-          SettingsSection(
-            title: const Text('My Activities'),
-            tiles: [
-              SettingsTile(
-                title: const Text('Left Swipes'),
-                leading: const Icon(Icons.arrow_back_ios),
-                onPressed: (BuildContext context) {
-                  context.push('/swipes?email=$email&direction=left');
-                },
-              ),
-              SettingsTile(
-                title: const Text('Right Swipes'),
-                leading: const Icon(Icons.arrow_forward_ios),
-                onPressed: (BuildContext context) {
-                  context.push('/swipes?email=$email&direction=right');
-                },
-              ),
-              SettingsTile(
-                title: const Text('Top Swipes'),
-                leading: const Icon(Icons.arrow_upward_outlined),
-                onPressed: (BuildContext context) {
-                  context.push('/swipes?email=$email&direction=top');
-                },
-              ),
-              SettingsTile(
-                title: const Text('My Requests'),
-                leading: const Icon(Icons.person_add),
-                onPressed: (BuildContext context) {
-                  context.push('/incoming/swipes?email=$email');
-                },
-              ),
-              SettingsTile(
-                title: const Text('My Contacts'),
-                leading: const Icon(Icons.group),
-                onPressed: (BuildContext context) {
-                  context.push('/contacts?email=$email');
-                },
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: const Text('Account Details'),
-            tiles: [
-              SettingsTile(
-                title: const Text('Security'),
-                leading: const Icon(Icons.lock),
-                onPressed: (BuildContext context) {},
-              ),
-            ],
-          ),
-        ],
-      ),
-      bottomNavigationBar: DigBottomNavBar(email: email),
-    );
+    bool isLoggedIn = provider.isLoggedIn;
+    if (isLoggedIn) {
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Icon(Icons.settings),
+        ),
+        body: SettingsList(
+          sections: [
+            SettingsSection(
+              title: const Text('My Activities'),
+              tiles: [
+                SettingsTile(
+                  title: const Text('Left Swipes'),
+                  leading: const Icon(Icons.arrow_back_ios),
+                  onPressed: (BuildContext context) {
+                    context.push('/swipes?email=$email&direction=left');
+                  },
+                ),
+                SettingsTile(
+                  title: const Text('Right Swipes'),
+                  leading: const Icon(Icons.arrow_forward_ios),
+                  onPressed: (BuildContext context) {
+                    context.push('/swipes?email=$email&direction=right');
+                  },
+                ),
+                SettingsTile(
+                  title: const Text('Top Swipes'),
+                  leading: const Icon(Icons.arrow_upward_outlined),
+                  onPressed: (BuildContext context) {
+                    context.push('/swipes?email=$email&direction=top');
+                  },
+                ),
+                SettingsTile(
+                  title: const Text('My Requests'),
+                  leading: const Icon(Icons.person_add),
+                  onPressed: (BuildContext context) {
+                    context.push('/incoming/swipes?email=$email');
+                  },
+                ),
+                SettingsTile(
+                  title: const Text('My Contacts'),
+                  leading: const Icon(Icons.group),
+                  onPressed: (BuildContext context) {
+                    context.push('/contacts?email=$email');
+                  },
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: const Text('App Settings'),
+              tiles: [
+                SettingsTile(
+                    title: const Text('Sign Out'),
+                    leading: const Icon(Icons.power_settings_new_outlined),
+                    onPressed: (BuildContext context) async {
+                      await FirebaseAuth.instance
+                          .signOut()
+                          .then((value) => ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                                  content: Text('User logged out'))))
+                          .then((value) => provider.checkFirebaseAuth())
+                          .then((value) => provider.onLogout())
+                          .then((value) => Navigator.popUntil(
+                              context, ModalRoute.withName('/login')));
+                    }),
+              ],
+            ),
+          ],
+        ),
+        bottomNavigationBar: DigBottomNavBar(email: email),
+      );
+    } else {
+      const CircularProgressIndicator();
+
+      return const LoginScreen();
+    }
   }
 }
